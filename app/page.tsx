b87461d7d6d28a56af73b482e16f93157c632156
Mocarram Hossain <mocarramhossain@gmail.com>
2025-06-17 06:31:02 +0600

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -390,8 +390,8 @@ export default function WorldClock() {
             </div>
 
             {/* Additional Timezone Cards Skeleton */}
-            <div className="space-y-6">
-              {[...Array(2)].map((_, i) => (
+            <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 mb-12">
+              {[...Array(6)].map((_, i) => (
                 <div key={i} className="glass-card rounded-2xl p-6 min-h-[200px] flex flex-col">
                   <div className="space-y-6">
                     {/* Header Skeleton */}
